e8bbeb7659fcccd7a2fb9a552730a767092d6789
Khiew Jian Bin <khiewbin@gmail.com>
2025-11-13 10:49:42 +0800

    adds bulleye

---
+++ Assets/UnityImageEffects/CirclesAnimated/GLSL/BullsEyeOnMouse/BullsEyeOnMouse.shader
@@ -0,0 +1,49 @@
+﻿Shader "GLSL/BullsEyeOnMouse" 
+{
+	Properties
+	{
+		mouse ("Mouse", Vector) = (0.5,0.5,0,0)
+	}
+	SubShader
+	{	
+		Pass
+		{
+			GLSLPROGRAM // Begin GLSL
+
+			#ifdef GL_ES
+			precision mediump float;
+			#endif
+
+			uniform vec2 mouse;
+			uniform vec4 _Time;
+
+			#ifdef VERTEX // Begin vertex program/shader
+
+			out vec4 texcoord;
+
+			void main()
+			{
+				gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex; // Draws the Vertex at the correct position in world
+
+				texcoord = gl_MultiTexCoord0; // Pass UV
+			}
+
+			#endif // Ends vertex program/shader
+
+			#ifdef FRAGMENT // Begin fragment program/shader
+
+			in vec4 texcoord;
+
+			void main()
+			{
+				float color = sin(distance(texcoord.xy, mouse.xy ) * 100 - _Time.y);
+
+				gl_FragColor = vec4(color, color, color, 1.0);
+			}
+
+			#endif // Ends fragment program/shader
+
+			ENDGLSL // End GLSL
+		}
+	}
+}
---
+++ Assets/UnityImageEffects/ShaderUtils/InjectMouse.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Script to inject mouse in the material in play mode
+*/
+
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InjectMouse : MonoBehaviour
+{
+    Material lastSharedMaterial;
+    void Update()
+    {
+        Ray r = RectTransformUtility.ScreenPointToRay(Camera.main, Mouse.current.position.ReadValue());
+        if (Physics.Raycast(r, out var hitinfo))
+        {
+            var hitpos = hitinfo.textureCoord;
+
+            var renderer = GetComponent<Renderer>();
+            if (renderer == null) return;
+            lastSharedMaterial = renderer.sharedMaterial;
+            if (lastSharedMaterial == null) return;
+
+            lastSharedMaterial.SetVector("mouse", new Vector4(hitpos.x, hitpos.y, 0, 0));
+        }
+        else if (lastSharedMaterial != null)
+        {
+            lastSharedMaterial.SetVector("mouse", new Vector4(0.5f, 0.5f, 0, 0));
+            lastSharedMaterial = null;
+        }
+    }
+}
